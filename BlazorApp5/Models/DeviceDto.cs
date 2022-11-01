ce1d9931d57630ea4e1cc4285c574b7ff10802fe
MayhemCoder <mayhemfn22@gmail.com>
2022-11-01 10:56:55 +0100

    Přidejte soubory projektu.

---
+++ BlazorApp5/Models/DeviceDto.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorApp5.Models
+{
+    public class DeviceDto
+    {
+        
+        [JsonPropertyName("BattVoltage")]
+        public double BattVoltage { get; set; }
+
+        [JsonPropertyName("ConcentratorId")]
+        public int ConcentratorId { get; set; }
+
+        [JsonPropertyName("DeviceId")]
+        public int DeviceId { get; set; }
+
+        [JsonPropertyName("IsLocked")]
+        public bool IsLocked { get; set; }
+
+        [JsonPropertyName("LastMsgReceived")]
+        public string LastMsgReceived { get; set; }
+
+        [JsonPropertyName("LockUnlockTag")]
+        public string LockUnlockTag { get; set; }
+
+        [JsonPropertyName("LockerName")]
+        public string LockerName { get; set; }
+
+        [JsonPropertyName("LockerNumber")]
+        public int LockerNumber { get; set; }
+    }
+}
---
+++ BlazorApp5/Program.cs
@@ -0,0 +1,38 @@
+using BlazorApp5.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+// There is a Dependency Injection containter where you have to register services
+// When you see this error - There is no registered service of type 
+// They are registered before Build of course :D
+
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
